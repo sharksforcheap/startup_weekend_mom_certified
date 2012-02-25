8d7a32b31bcb5ce200bbfa1a3d77679f678c409c
Anthony Phillips <anthony.phillips@gmail.com>
2012-02-25 13:10:30 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MomAndDad::Application.config.secret_token = 'f22d244348b37d91582aa334deb44425c0c9013922c823a3756dda42f32c1e87b682cb0b447e2c7c44d1d0f0d43405539fa48c5f8b94bd6076235c83e91e1afa'
