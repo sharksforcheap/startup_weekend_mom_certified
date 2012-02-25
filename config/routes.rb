52ca2070e89d784450449262b806ac9e862c65e0
Anthony Phillips <anthony.phillips@gmail.com>
2012-02-25 14:26:06 -0800

    maybe finally working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MomAndDad::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ MomAndDad::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
