132798c26b8d4372594e64abd4e0c9155ec8a683
Anthony Phillips <anthony.phillips@gmail.com>
2012-02-26 04:41:05 -0800

    fine tunes for heroku

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,12 @@
 class QuestionsController < ApplicationController
 	def show
-		@question = Question.find(params[:id])
 
-		@next_question = Question.find(params[:id].to_i + 1)
+		@question = Question.find(params[:id])
+		unless @quesion.id == 5
+			@next_question = Question.find(params[:id].to_i + 1)
+		else
+			@next_question = @question
+		end
 	end
 
 
